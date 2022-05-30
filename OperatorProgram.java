a76961b5a5207e316cac47fd4fb73fc0778d564c
SAHIL0455 <sahilpawar1004@gmail.com>
2022-05-30 11:23:53 +0530

    Find the Min and Max

--- OperatorProgram.java
+++ OperatorProgram.java
@@ -15,8 +15,25 @@ public class OperatorProgram {
         double second = c+a/b;
         double third = a%b+c;
         double fourth = a*b+c;
-        if (first > second || first > third || first > fourth){
-            System.out.println(" a+b*c is max");
+        if (first > second && first > third && first > fourth){
+            System.out.println("a+b*c is max");
+        }else if (second > first && second > third && second > fourth) {
+            System.out.println("c+a/b is max");
+        }else if (third > first && third > second && third > fourth) {
+            System.out.println("a%b+c is max");
+        }else if (fourth > first && fourth > third && fourth >second) {
+            System.out.println("a*b+c is max");
+        }
+
+        if (first < second && first < third && first < fourth) {
+            System.out.println("a+b*c is min");
+        }
+         else if (second < first && second < third && second < fourth) {
+            System.out.println("c+a/b is min");
+        }else if (third < first && third < second && third < fourth) {
+            System.out.println("a%b+c is min");
+        }else if (fourth < first && fourth < third && fourth < second) {
+            System.out.println("a*b+c is min");
         }
     }
 }
